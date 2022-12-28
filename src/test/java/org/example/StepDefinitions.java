8576f27c6e58836bb7f4302dd815633af71e4ce7
Solovey96 <garret-tm@yandex.ru>
2022-12-28 16:34:47 +0500

    done_mk1_skill

---
+++ src/test/java/org/example/StepDefinitions.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class StepDefinitions {
+    public static final WebDriver webDriver;
+    static {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\garre\\IdeaProjects\\chromedriver.exe");
+    webDriver = new ChromeDriver();
+    webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+    }
+
+    @Given("url of platform {string}")
+    public void url_of_platform(String url) {
+        webDriver.get(url);
+    }
+    @Then("enter name {string}")
+    public void enter_name(String name) {
+        WebElement webElement = webDriver.findElement(By.name("name"));
+        webElement.sendKeys(name);
+    }
+    @Then("enter email {string}")
+    public void enter_email(String email) {
+        WebElement webElement = webDriver.findElement(By.name("email"));
+        webElement.sendKeys(email);
+    }
+    @Then("enter phone {string}")
+    public void enter_phone(String phone) {
+        WebElement webElement = webDriver.findElement(By.xpath("//*[@id=\"form456746058\"]/div[2]/div[3]/div/div[1]/input[1]"));
+        webElement.sendKeys(phone);
+    }
+    @And("corrected")
+    public void corrected() {
+        webDriver.findElement(By.xpath("//*[@id=\"form456746058\"]/div[2]/div[5]/button")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"tildaformsuccesspopup\"]/div/div"));
+    }
+    @And("incorrect")
+    public void incorrect() {
+        webDriver.findElement(By.xpath("//*[@id=\"form456746058\"]/div[2]/div[5]/button")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"tilda-popup-for-error\"]"));
+    }
+
+}
